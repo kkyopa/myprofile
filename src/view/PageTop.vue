ee0d2b7853871f4daf1d34d65bba0a67fbcb1997
kawada.kyosuke.mc00359@memberscareer.co.jp <kawada.kyosuke.mc00359@memberscareer.co.jp>
2020-08-19 15:47:28 +0900

    firstpush

---
+++ src/view/PageTop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="toppage">
+    <h2>川田京助</h2>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.toppage {
+  background-color: black;
+  min-height: 800px;
+  margin: 0;
+}
+
+.toppage > h2 {
+  margin:0;
+  color:white;
+  line-height:800px;
+}
+</style>
