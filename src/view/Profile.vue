dac5a76c5584852fa8dd3a832c90416ee96447ef
kawada.kyosuke.mc00359@memberscareer.co.jp <kawada.kyosuke.mc00359@memberscareer.co.jp>
2020-08-21 18:12:54 +0900

    アイコンをプロフィールに移動

--- src/view/Profile.vue
+++ src/view/Profile.vue
@@ -15,7 +15,35 @@
     </p>
     <p>
       LaravelとVue.jsに興味あり
-    </p>
+    </p><br>
+
+          <div class="container">
+      <div class="icon">
+        <img src="../img/suit.png" width="120" height="120" alt="太郎のアイコンです">
+      </div>
+      <div class="info">
+        <h3>Kyousuke Kawada</h3>
+        <ul>
+          <li>
+            <a href="https://github.com/kkyopa" target="_blank">
+              <img src="../img/github.png" width="20" height="20" alt="github">
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/ka.wa.da.kyosuke" target="_blank">
+              <img src="../img/facebook.png" width="20" height="20" alt="facebook">
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/kazumons" target="_blank">
+              <img src="../img/twitter.png" width="20" height="20" alt="facebook">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
@@ -39,6 +67,48 @@
 
 .profile p {
   color: black;
+}
+
+
+.container {
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  align-items: center;
+}
+
+.icon img {
+  border-radius: 50%;
+  border-width: 4px;
+  border-style: solid;
+  border-color:#228B22;
+  margin-left:560px;
+}
+
+.info {
+  margin-left:1px;
+}
+
+.info h3 {
+  font-family: "Brandon Grotesque W01", "Century Gothic", "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN W3", Meiryo, メイリオ, sans-serif;
+  margin: 0;
+  color:white;
+  padding-left:17px;
+}
+
+.info p {
+  margin: 0;
+  margin-bottom: 16px;
+}
+
+.info ul {
+  list-style-type: none;
+  display: flex;
+  padding-left: 50px;
+}
+
+.info ul li {
+  margin-right: 26px;
 }
 
 </style>
