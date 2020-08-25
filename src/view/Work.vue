57eee2d47a177db0df0cbf0f3c8d8bd47eb5ebc0
kawada.kyosuke.mc00359@memberscareer.co.jp <kawada.kyosuke.mc00359@memberscareer.co.jp>
2020-08-25 18:08:42 +0900

    モーダル機能作成

--- src/view/Work.vue
+++ src/view/Work.vue
@@ -1,25 +1,61 @@
 <template>
   <div class="work">
     <h1>Portfolio</h1>
-    <div class="portfolio">
-        <div class="sample_box"><img src="../assets/img/itiun.png" alt="" width="300" height="200"></div>
-        <h3>一日一膳一運</h3>
-        <p>運を可視化するアプリ</p>
-    </div>
-    <div class="portfolio">
-        <div class="sample_box"><img src="../assets/img/arrange.png" alt="" width="300" height="200"></div>
-        <h3>DartsArrange</h3>
-        <p>ダーツの01ゲームによる攻略アプリ</p>
-    </div>
-        <div class="portfolio">
-        <div class="sample_box"><img src="../assets/img/po.png" alt="" width="300" height="200"></div>
-        <h3>ポートフォリオサイト</h3>
-        <p>自身のWebサイトです</p>
-    </div>
+    <h3>過去の制作事例の一部を紹介します。</h3>
+      <div class="portfolio" v-for='(item, items) in items' :key='items'>
+        <div class="sample_box">
+          <img  @click="openModal(item)" :src="item.img_src" alt="Card image" width="300" height="200">
+        </div>
+        <modal :val="postItem" v-show="showContent" @close="closeModal" />
+      </div>
   </div>
 </template>
 
-<script></script>
+<script>
+  import Modal from './Modal.vue'
+  export default {
+    components: {
+      Modal
+    },
+    data () {
+      return {
+        showContent: false,
+        postItem: "",
+        items: [
+          { id: 1,
+            desc: '課題で作成した美容室のHPです。',
+            range: 'コーディング（レスポンシブ対応）',
+            lang: 'HTML, CSS(Sass)',
+            img_src: require('../assets/img/itiun.png'),
+          },
+          {
+            id: 2,
+            desc: '実案件で作成したコーポレートサイトです。',
+            range: 'コーディング（レスポンシブ対応）',
+            lang: 'HTML, CSS(Sass)',
+            img_src: require('../assets/img/arrange.png'),
+          },
+                    {
+            id: 3,
+            desc: '実案件で作成したコーポレートサイトです。',
+            range: 'コーディング（レスポンシブ対応）',
+            lang: 'HTML, CSS(Sass)',
+            img_src: require('../assets/img/po.png'),
+          },
+        ]
+      }
+    },
+    methods: {
+       openModal(item) {
+        this.showContent = true
+        this.postItem = item
+      },
+      closeModal () {
+        this.showContent = false
+      }
+    }
+  }
+</script>
 
 <style>
 .work {
@@ -31,22 +67,26 @@
 
 .work > h1 {
   margin: 0;
-  line-height: 200px;
+  line-height: 100px;
   font-size: 52px;
   font-family: "Brandon Grotesque W01", "Century Gothic",
     "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN W3", Meiryo, メイリオ,
     sans-serif;
 }
 
+.work > h3 {
+  margin-bottom:100px;
+}
+
 .portfolio {
  float: left;
- height:650px;
+ margin-bottom:400px;
 }
 
 .sample_box{
  width:470px;
  margin:0 auto;
- 
+
 }
 .sample_box img{
  transition:1s all;
